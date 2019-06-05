804cbb6fe19e18a0cd3b8a86dd57ae815f0371ec
trk <dev.trk.9001@gmail.com>
2019-06-05 22:44:31 +0600

    Create a basic program template

---
+++ templates/template.cpp
@@ -0,0 +1,9 @@
+#include "bits/stdc++.h"
+#define rep(i,n) for(int i=0;i<(n);++i)
+#define rep1(i,p,q) for(int i=(p);i<=(q);++i)
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+}
