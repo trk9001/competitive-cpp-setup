f4fc3f9967808984455847c8420169f266c2fb7e
trk <dev.trk.9001@gmail.com>
2019-06-06 18:54:10 +0600

    Add macro for reverse-sorting a vec

--- templates/template.cpp
+++ templates/template.cpp
@@ -7,6 +7,8 @@
 // Vector macros
 #define vi vector<int>
 #define visum(v) accumulate((v).begin(),(v).end(),0)
+#define vsort(v) sort((v).begin(),(v).end())
+#define vsortr(v) sort((v).rbegin(),(v).rend())
 
 using namespace std;
 
