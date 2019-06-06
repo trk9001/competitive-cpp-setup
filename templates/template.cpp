a2546a7f1f650db8114bcbe284ae93603ad7c29f
trk <dev.trk.9001@gmail.com>
2019-06-06 18:31:23 +0600

    Update vector macros and spacing

--- templates/template.cpp
+++ templates/template.cpp
@@ -1,7 +1,13 @@
 #include "bits/stdc++.h"
+
+// Loops
 #define rep(i,n) for(int i=0;i<(n);++i)
 #define rep1(i,p,q) for(int i=(p);i<=(q);++i)
-#define veci vector<int>
+
+// Vector macros
+#define vi vector<int>
+#define visum(v) accumulate((v).begin(),(v).end(),0)
+
 using namespace std;
 
 int main() {
